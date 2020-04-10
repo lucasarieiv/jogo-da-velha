70c0d2644c73a9e2f4a393b96c43154673920158
lucasarieiv <w3desenvolvedor@gmail.com>
2020-04-10 13:01:35 -0300

    Aula 049

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <a className="link-item" href="#">Sobre</a>
-    </div>
+    <a href="#to-do" className="action">Sobre</a>
   );
 }
 
