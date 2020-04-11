46998db04d2686cb7874cca5f9e56cbfcb85be7c
lucasarieiv <w3desenvolvedor@gmail.com>
2020-04-11 17:43:12 -0300

    Desafio Intermediario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sobre from './objects/Sobre';
 import Logo from './objects/Logo';
+import Menu from './objects/Menu'
 
 function App() {
     return (
-        <Logo />
+        // <Sobre />
+        <Menu />
     );
 }
 
---
+++ src/objects/Menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const Menu = () => {
+    return (
+        <div className="menu-hamburguer">Menu</div>
+    )
+}
+
+export default Menu
