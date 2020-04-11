f299c892da23dd696770c276632a499a871370c6
lucasarieiv <w3desenvolvedor@gmail.com>
2020-04-11 17:35:21 -0300

    Desafio Basico

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Sobre from './objects/Sobre';
+import Logo from './objects/Logo';
 
 function App() {
     return (
-        <Sobre />
+        <Logo />
     );
 }
 
---
+++ src/objects/Logo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.png';
+
+const Logo = () => {
+    return (
+        <img src={logo}></img>
+    )
+}
+
+export default Logo;
