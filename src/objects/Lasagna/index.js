ee6affe178b94bbe2ac59f4a30dcca1945880d81
lucasarieiv <w3desenvolvedor@gmail.com>
2020-04-14 15:47:17 -0300

    Excluindo arquivo

---
+++ src/objects/Lasagna/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+
+const Lasagna = () => (
+    <div className="lasagna">
+        <div className="top top-left"></div>
+        <div className="top top-center"></div>
+        <div className="top top-right"></div>
+
+        <div className="center center-left"></div>
+        <div className="center center-center"></div>
+        <div className="center center-right"></div>
+
+        <div className="bottom bottom-left"></div>
+        <div className="bottom bottom-center"></div>
+        <div className="bottom bottom-right"></div>
+    </div>
+)
+
+export default Lasagna;
