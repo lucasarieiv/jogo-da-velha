5b0d2599fb8b28bc084c546aedfdcd3600c2b9ba
lucasarieiv <w3desenvolvedor@gmail.com>
2020-04-17 17:31:09 -0300

    Desafio basico - Aula 051

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
+import Events from './components/Events';
 
 const App = () =>  (
         <main className="app">
             <Header />
             <Hashtag />
+            <Events />
         </main>
 )
 
---
+++ src/objects/InputBox/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InputBox = () => (
+    <input type="checkbox" name="" id=""/>
+)
+
+export default InputBox;
