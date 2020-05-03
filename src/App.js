3c35536f8a14c7eeacec828b6c0d885c6149f1e1
lucasarieiv <you@example.com>
2020-05-03 20:47:24 -0300

    Criamos o before e after do segundo item da nossa hashtag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
+
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
-import Events from './components/Events';
+import Checkbox from './objects/Checkbox';
 
 const App = () =>  (
         <main className="app">
             <Header />
             <Hashtag />
-            <Events />
+            <Checkbox id="show" value="show" content="Mostrar eventos" />
         </main>
 )
 
--- src/objects/InputBox/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const InputBox = () => (
-    <input type="checkbox" name="" id=""/>
-)
-
-export default InputBox;
