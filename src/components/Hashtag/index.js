0134c12dac059c41a25c9bfbaae8524469f7661f
Lucas Vieira <lucasvieira@luttos.com>
2023-01-20 23:42:37 -0300

    Verificando jogadas e apontando ganhador

--- src/components/Hashtag/index.js
+++ src/components/Hashtag/index.js
@@ -5,43 +5,98 @@ import Player from '../../objects/Player';
 
 const Hashtag = () => {
     const [turn, setTurn] = useState('x');
+    const [move, setMove] = useState(0);
+
+    let winnerMap = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        
+        [0,4,8],
+        [2,4,6]
+    ]
+
     const [hashGame, setHashGame] = useState(
         [
-            {play: ''},
-            {play: ''},
-            {play: ''},
+            {play: '', playIndex: 0},
+            {play: '', playIndex: 0},
+            {play: '', playIndex: 0},
     
-            {play: ''},
-            {play: ''},
-            {play: ''},
+            {play: '', playIndex: 0},
+            {play: '', playIndex: 0},
+            {play: '', playIndex: 0},
     
-            {play: ''},
-            {play: ''},
-            {play: ''},
+            {play: '', playIndex: 0},
+            {play: '', playIndex: 0},
+            {play: '', playIndex: 0},
         ]
     )
 
-    function handlePlay(index) {
-        if (turn === 'x') {
-            setHashGame((old) => {
-                old[index].play = turn;
-                return old;
+    function verifyWinner() {
+        for (let winnerSequence of winnerMap) {
+            let countX = 0;
+            let countO = 0;
+            winnerSequence.forEach(position => {
+                if (hashGame[position].play === 'x') {
+                    countX += 1;
+                } else if (hashGame[position].play === 'o') {
+                    countO += 1;
+                }
             })
-            setTurn('o')
-        } else {
-            setHashGame((old) => {
-                old[index].play = turn;
-                return old;
-            })
-            setTurn('x')
+            if (countX === 3) {
+                console.log('X vencedor')
+                break;
+            } else if (countO === 3) {
+                console.log('O Vencedor')
+            }
+
         }
     }
+
+    function setMoviment() {
+        let count = move + 1;
+        setMove(count);
+    }
+
+    function verifyMovimentCount() {
+        if (move >= 5) {
+            verifyWinner()
+        }
+    }
+
+    const handlePlay = ({target}, index) => {
+        const moveIsEmpty = hashGame[index].play;
+
+        if (moveIsEmpty) return;
+        
+        setHashGame((old) => {
+            old[index].play = turn;
+            old[index].playIndex = index;
+            return old;
+        })
+
+        if (turn === 'x') {
+            setTurn('o')
+        } else {
+            setTurn('x')
+        }
+
+        setMoviment()
+
+    }
+
+    verifyMovimentCount()
+
 
     return (
         <Card>
             <ul className="hashtag">
                 {hashGame.map(({play}, index) => 
-                    <li key={index} className="item" onClick={() => handlePlay(index)}><Player player={play} /></li>
+                    <li key={index} className="item" onClick={(e) => handlePlay(e, index)}><Player player={play} /></li>
                 )}
             </ul>
         </Card>
